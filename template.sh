e8f88c9869148affb9b792cf8cf5c0458ff202dc
Andrej Lamov <andrej.lamov@gmail.com>
2019-09-02 22:00:07 +0200

    Use \ in sed to substitute paths

--- template.sh
+++ template.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 ROOT=$(readlink -f $1)
 ROOT_LEN=$(expr length "$ROOT" + 2)
@@ -8,14 +9,14 @@ gen_sed() {
   for k in "$@"
   do
     v="${!k}"
-    EXPR="$EXPR -e 's/%$k%/$v/g'"
+    EXPR="$EXPR -e 's\\%$k%\\$v\\g'"
   done
   echo "$EXPR"
 }
 
 unique_vars() {
   GREP_EXPR="%.*?%"
-  SED_EXPR="s/%//g"
+  SED_EXPR="s\\%\\\\g"
 
   VARS_IN_PATHS=$(
     find "$ROOT" -type f | grep -ohP "$GREP_EXPR" | sed "$SED_EXPR")
@@ -27,6 +28,24 @@ unique_vars() {
     | sort -u
 }
 
+validate_all_set() {
+  VARS=$1
+
+  VAR_NOT_SET=0
+  for K in $VARS
+  do
+  V="${!K}"
+  if [ -z "$V" ]; then
+      VAR_NOT_SET=1
+  fi
+  echo "  $K=$V"
+  done
+  if [ $VAR_NOT_SET -eq "1" ]; then
+      echo "All variables not set, aborting"
+      exit
+  fi
+}
+
 UNIQUE_VARS=$(unique_vars)
 
 echo "Source template:"
@@ -36,24 +55,21 @@ echo "Target:"
 echo "  $(pwd)"
 echo "Variables:"
 
-for k in $UNIQUE_VARS
-do
-  v="${!k}"
-  echo "  $k=$v"
-done
+validate_all_set "$UNIQUE_VARS"
 
 SED_EXPR=$(gen_sed $UNIQUE_VARS)
-
+echo "Using sed:"
+echo "  $SED_EXPR"
 echo "Copying:"
 for FILE in $(find $ROOT -type f)
 do
   OLD_FILE=$(echo "$FILE" \
                | cut "-c$ROOT_LEN-")
-  NEW_FILE=$(echo "$FILE" \
-               | eval "sed $SED_EXPR" \
-               | cut "-c$ROOT_LEN-")
-  mkdir -p "$(dirname "$NEW_FILE")"
 
+  NEW_FILE=$(echo "$FILE" \
+               | eval sed "$SED_EXPR" \
+               | cut "-c$ROOT_LEN-")
   echo "  $OLD_FILE -> $NEW_FILE"
+  mkdir -vp "$(dirname "$NEW_FILE")"
   eval "sed $SED_EXPR $FILE > $NEW_FILE"
 done
